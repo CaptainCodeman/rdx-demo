833ccf5aba94fefc291b61b90e687671d0a4c996
Simon Green <simon@captaincodeman.com>
2020-04-06 09:22:54 -0600

    Add dev tools (build and serve)

---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+'use strict';
+
+import resolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+import alias from '@rollup/plugin-alias'
+import minify from 'rollup-plugin-minify-html-literals'
+import { terser } from 'rollup-plugin-terser'
+import size from 'rollup-plugin-size'
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'public/scripts/app.js',
+    format: 'esm',
+    sourcemap: true,
+  },
+  plugins: [
+    alias({
+      entries: [{
+        find: 'lit-html/lib/shady-render.js',
+        replacement: 'node_modules/lit-html/lit-html.js',
+      }]
+    }),
+    resolve({
+      dedupe: [
+        'lit-element',
+        'lit-html',
+      ]
+    }),
+    production && minify(),
+    typescript({ typescript: require('typescript') }),
+    production && terser({
+      output: {
+        comments: false
+      }
+    }),
+    production && size(),
+  ]
+}
---
+++ server.js
@@ -0,0 +1,25 @@
+const browserSync = require('browser-sync').create();
+const historyApiFallback = require('connect-history-api-fallback');
+const logger = require('connect-logger');
+const compression = require('compression');
+
+browserSync.init({
+  cwd: 'public',
+  server: {
+    baseDir: 'public',
+    index: 'index.html',
+    routes: {
+      '/node_modules': 'node_modules',
+      '/src': 'src',
+  }
+  },
+  files: [
+    'scripts/**',
+    'index.html',
+  ],
+  middleware: [
+    logger(),
+    compression({ level: 9 }),
+    historyApiFallback(),
+  ],
+});
